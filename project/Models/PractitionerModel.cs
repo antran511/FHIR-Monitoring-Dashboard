b7136c4d1ab0cf49c2296c68d0b63a8a9d56d6ae
An Kien Tran <ktra0013@student.monash.edu>
2020-05-07 11:01:32 +1000

    Initalize project structure

---
+++ project/Models/PractitionerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hl7.Fhir.Model;
+
+namespace FHIR_FIT3077.Models
+{
+    public class PractitionerModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ project/Repository/PractitionerRepository.cs
@@ -0,0 +1,19 @@
+﻿using FHIR_FIT3077.IRepository;
+using Hl7.Fhir.Model;
+
+namespace FHIR_FIT3077.Repository
+{
+    public class PractitionerRepository : IPractitionerRepository
+    {
+        private readonly IPractitionerRepository _pracrepository;
+        public PractitionerRepository(IPractitionerRepository pracrepository)
+        {
+            _pracrepository = pracrepository;
+        }
+
+        public Practitioner GetPractitioner(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
