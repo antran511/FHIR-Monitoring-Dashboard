c1f5328521bc803bd6b05ecf087e17c2e6a2d1fd
Linh Le <llee0010@student.monash.edu>
2020-05-10 15:54:03 +1000

    second fix

--- project/Models/PatientModel.cs
+++ project/Models/PatientModel.cs
@@ -7,6 +7,8 @@ namespace FHIR_FIT3077.Models
 {
     public class PatientModel
     {
+        public string Id { get; set; }
+
         public string Name { get; set; }
     }
 }
--- project/Models/PractitionerModel.cs
+++ project/Models/PractitionerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Hl7.Fhir.Model;
@@ -8,6 +9,8 @@ namespace FHIR_FIT3077.Models
 {
     public class PractitionerModel
     {
+        [Required]
         public string Id { get; set; }
+
     }
 }
