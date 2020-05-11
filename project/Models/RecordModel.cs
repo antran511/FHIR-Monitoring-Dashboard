c5f05d4af824f5f0703ca214a10e2f468105ea65
An Kien Tran <ktra0013@student.monash.edu>
2020-05-11 16:46:22 +1000

    getting patient's cholesterol

---
+++ project/Models/RecordModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FHIR_FIT3077.Models
+{
+    public class RecordModel
+    {
+        public string Cholesterol { get; set; }
+        public string Date { get; set; }
+
+    }
+}
